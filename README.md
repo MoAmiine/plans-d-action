290a9d02d786cab905bc8364cea8b367f690810f
Moamiine <medamine.teljaoui@gmail.com>
2025-12-01 20:03:52 +0100

    manipulation des tableau partie 1

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # plans-d-action
+## MANIPULATION DES TABLEAUX :
+### Partie 1 :
+ [] : pour creer un tableau vide d'une maniere simple 
+ new Array() : fonction predefinie pour creer un tableau
+ .lenght : nombre d'elements dans le tableau 
--- script.js
+++ script.js
@@ -1,15 +1,7 @@
 // exercice 1
-// console.log(localStorage);
-// localStorage.setItem("name", "Ali")
-// localStorage.getItem("name")
-
-// exercice 2
-localStorage.setItem("name", "Ali")
-console.log(localStorage.getItem("name"))
-console.log()
-localStorage.removeItem("name")
-for (let i = 0; i < localStorage.length; i++) {
-    console.log(localStorage.key(i))
-}
+let tableau = ["amine", "mohamed", "omar", "aymane", "hamza"]
+console.log(tableau[0])
+console.log(tableau[tableau.length - 1])
+tableau[2] = "simo"
 
 
