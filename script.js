cf8ddf1b22556f6b5917859d8900018edf9f940a
Moamiine <medamine.teljaoui@gmail.com>
2025-12-01 12:18:13 +0100

    exercice 2 localstorage

--- script.js
+++ script.js
@@ -1,4 +1,15 @@
 // exercice 1
-console.log(localStorage);
+// console.log(localStorage);
+// localStorage.setItem("name", "Ali")
+// localStorage.getItem("name")
+
+// exercice 2
 localStorage.setItem("name", "Ali")
-localStorage.getItem("name")
+console.log(localStorage.getItem("name"))
+console.log()
+localStorage.removeItem("name")
+for (let i = 0; i < localStorage.length; i++) {
+    console.log(localStorage.key(i))
+}
+
+
