0797727afd241c0c985e6b350afbde76daf2609c
Moamiine <medamine.teljaoui@gmail.com>
2025-12-01 20:20:45 +0100

    manipulation des tableau partie 2

--- script.js
+++ script.js
@@ -1,7 +1,12 @@
 // exercice 1
-let tableau = ["amine", "mohamed", "omar", "aymane", "hamza"]
-console.log(tableau[0])
-console.log(tableau[tableau.length - 1])
-tableau[2] = "simo"
+// let tableau = ["amine", "mohamed", "omar", "aymane", "hamza"]
+// console.log(tableau[0])
+// console.log(tableau[tableau.length - 1])
+// tableau[2] = "simo"
 
-
+// exercice 2
+let tableau = ["amine", "omar", "hamza"]
+tableau.push("mohamed")
+tableau.unshift("aymen")
+tableau.shift()
+tableau.pop()
